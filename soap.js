cf66d472b00bceebfb6c27d5ae6e94891cc8295a
David Lemvigh <dlemvigh@gmail.com>
2016-07-18 19:43:08 +0200

    node.js soap server

---
+++ soap.js
@@ -0,0 +1,61 @@
+var http = require('http');
+var soap = require('soap');
+
+var xml = require('fs').readFileSync('private/HelloService.wsdl', 'utf8'),
+    server = http.createServer(function(request,response) {
+        response.end("404: Not Found: " + request.url);
+    });
+
+
+
+var myService = {
+    Hello_Service: {
+        Hello_Port: {
+            sayHello: function(args) {
+                console.log("called")
+                return {
+                    greeting: args.firstName,
+                    sender: 'node',
+                    timestamp: Date()
+                }
+            }
+        }
+    },
+    MyService: {
+        MyPort: {
+            MyFunction: function(args) {
+                return {
+                    name: args.name
+                };
+            },
+
+            // This is how to define an asynchronous function.
+            MyAsyncFunction: function(args, callback) {
+                // do some work
+                callback({
+                    name: args.name
+                });
+            },
+
+            // This is how to receive incoming headers
+            HeadersAwareFunction: function(args, cb, headers) {
+                return {
+                    name: headers.Token
+                };
+            },
+
+            // You can also inspect the original `req`
+            reallyDetailedFunction: function(args, cb, headers, req) {
+                console.log('SOAP `reallyDetailedFunction` request from ' + req.connection.remoteAddress);
+                return {
+                    name: headers.Token
+                };
+            }
+        }
+    }
+};
+
+server.listen(8000);
+soap.listen(server, '/wsdl', myService, xml);
+
+console.log("started")
